8743ff734533ff56a46bd234fdfbf271f1051ac6
jhiney <56890661+jhiney@users.noreply.github.com>
2020-11-30 14:46:38 -0600

    get react working from create react app

--- bluephatreact/src/App.js
+++ bluephatreact/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
+import { NavMenu } from './components/NavMenu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render(){
+    return(
+      <div>
+        <NavMenu />
+      </div>
+    );
+  }
 }
-
-export default App;
