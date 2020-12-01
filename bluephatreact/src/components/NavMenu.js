7aa1ea1a8656f9f694e5b0ef57ece8330411bfd2
jhiney <56890661+jhiney@users.noreply.github.com>
2020-12-01 15:57:04 -0600

    Full Navbar implemented in react

--- bluephatreact/src/components/NavMenu.js
+++ bluephatreact/src/components/NavMenu.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Dropdown, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse,
+         Container, 
+         UncontrolledDropdown,
+         DropdownItem, 
+         DropdownToggle,
+         DropdownMenu, 
+         Navbar, 
+         NavbarBrand, 
+         NavbarToggler, 
+         NavItem, 
+         NavLink, 
+         Form } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import logo from './layoutimages/headerlogoblue.png';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -21,22 +33,42 @@ export class NavMenu extends Component {
     });
   }
 
+  
+
   render () {
     return (
       <header>
-        <Navbar className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"#2199CD"}}>
+        <Navbar className="navbar-expand-lg navbar-light" style={{backgroundColor:"#2199CD"}}>
           <Container>
-            <NavbarBrand tag={Link} to="/">Accounting Toolbox</NavbarBrand>
+            <NavbarBrand tag={Link} to="/"><img src={logo} alt="headerlogo" /></NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
+            <Form className="form-inline my-2 my-lg-0 ml-auto">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+              </Form>
+            <ul class="navbar-nav mr-auto">
                 <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/accountportal">Acount Portal</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                   <Dropdown tag={Link} className="text-dark" to="/admininfo">Account Administration</Dropdown>
+                <NavLink tag={Link} className="text-dark" to="/blog">Blog</NavLink>
                 </NavItem>
-
+                <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/bis">BIS</NavLink>
+                </NavItem>
+                <NavItem>
+                  <UncontrolledDropdown>
+                    <DropdownToggle tag="a" className="nav-link" caret> Clans </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>Header</DropdownItem>
+                        <DropdownItem disabled>Action</DropdownItem>
+                        <DropdownItem>Another Action</DropdownItem>
+                          <DropdownItem divider />
+                          <DropdownItem>Another Action</DropdownItem>
+                      </DropdownMenu>
+                  </UncontrolledDropdown>
+                </NavItem>
               </ul>
             </Collapse>
           </Container>
@@ -45,3 +77,5 @@ export class NavMenu extends Component {
     );
   }
 }
+
+
