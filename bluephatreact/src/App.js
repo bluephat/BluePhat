864d1a70ad8c49a61d14d4ed445cdcf5067b8a27
jhiney <56890661+jhiney@users.noreply.github.com>
2020-12-06 17:34:13 -0600

    Update App.js
    whitespace delete

--- bluephatreact/src/App.js
+++ bluephatreact/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
 
-
-
 export default class App extends Component {
   static displayName = App.name;
 
