2d52676af420e7d5b2f2abec27d4319b3e3f099f
Jake Hiney <hiney.jake@outlook.com>
2021-01-24 11:41:59 -0600

    Implemented Card Deck
    implemented full card deck

--- bluephatreact/src/components/MainCards.js
+++ bluephatreact/src/components/MainCards.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import {
-  Card, Button, CardImg, CardTitle, CardText, CardDeck,
-  CardSubtitle, CardBody
+  Card,  CardImg,  CardText, CardDeck, CardBody
 } from 'reactstrap';
+import blog from './layoutimages/blogicon.png'
+import bis from './layoutimages/bisicon.png'
+import clan from './layoutimages/clansicon.png'
+import forum from './layoutimages/forum.png'
 
 export default class MainCards extends Component {
     static displayName = MainCards.name;
@@ -10,30 +13,27 @@ export default class MainCards extends Component {
   return (
     <CardDeck>
       <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardImg top width="100%" src={blog} alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
+            <CardText>Keep up to date with OSRS updates, events, and development feeds</CardText>
         </CardBody>
       </Card>
       <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardImg top width="100%" src= {bis}  alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
+            <CardText>Calculate best in slot equipment for your character specific combat skill levels.</CardText>
         </CardBody>
       </Card>
       <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardImg top width="100%" src={clan} alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
+            <CardText>Find OSRS communities that fit your interests, gameplay style, and region.</CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src={forum} alt="Card image cap" />
+        <CardBody>
+            <CardText>Chat with members of the community, and find answers to questions you might have.</CardText>
         </CardBody>
       </Card>
     </CardDeck>
